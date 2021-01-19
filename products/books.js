e83e4015ea6e04e268e429840d9c8b665e8001b8
Eion Nelson <ecnelson1@live.com>
2021-01-19 14:35:09 -0800

    added first product data in books.js

---
+++ products/books.js
@@ -0,0 +1,8 @@
+const midnightLib= {
+    id: 'The Midnight Library by Matt Haig',
+    name: 'The Midnight Library',
+    image: 'assets/midLib.png',
+    description: 'Between life and death there is a library, and within that library, the shelves go on forever. Every book provides a chance to try another life you could have lived. To see how things would be if you had made other choices . . . Would you have done anything different, if you had the chance to undo your regrets?',
+    category: 'feel-good',
+    price: 17.00
+}
