544a5eb4306c280f34b29a1aeea9323d4d19e3d4
Eion Nelson <ecnelson1@live.com>
2021-01-19 16:26:11 -0800

    added a static html model to get display elements for better testing of render function

--- products/books.js
+++ products/books.js
@@ -1,7 +1,7 @@
 const midnightLib = {
     id: 'The Midnight Library by Matt Haig',
     name: 'The Midnight Library',
-    image: 'assets/midLib.png',
+    image: '../assets/midLib.png',
     description: 'Between life and death there is a library, and within that library, the shelves go on forever. Every book provides a chance to try another life you could have lived. To see how things would be if you had made other choices . . . Would you have done anything different, if you had the chance to undo your regrets?',
     category: 'feel-good',
     price: 17.00
@@ -9,7 +9,7 @@ const midnightLib = {
 const nRBG = {
     id: 'Notorious RBG by Irin Carmon and Shana Knizhnik',
     name: 'Notorious RBG',
-    image: 'assets/RBG.png',
+    image: '../assets/RBG.png',
     description: 'This entertaining and insightful full-color 200-page young readers edition mixes pop culture, humor, and expert analysis for a remarkable account of the indomitable Ruth Bader Ginsburg: Heroine. Trailblazer. Pioneer.Supreme Court Justice Ruth Bader Ginsburg was an icon to millions. Her tireless fight for equality and womens rights inspired not only great strides in the workforce but impacted the law of the land.',
     category: 'Biographical-History',
     price: 16.50
@@ -17,7 +17,7 @@ const nRBG = {
 const buckWitch = {
     id: 'Bucklands Complete Book of Witchcraft by Raymond Buckland',
     name: 'Bucklands Complete Book of Witchcraft',
-    image: 'assets/buckWitch.png',
+    image: '../assets/buckWitch.png',
     description: 'Bucklands Complete Book of Witchcraft has influenced and guided countless students, coven initiates, and solitaries around the world. One of modern Wiccas most recommended books, this comprehensive text features a step-by-step course in Witchcraft, with photographs and illustrations, rituals, beliefs, history, and lore, as well as instruction in spellwork, divination, herbalism, healing, channeling, dreamwork, sabbats, esbats, covens, and solitary practice.',
     category: 'Religion',
     price: 22.00
@@ -25,7 +25,7 @@ const buckWitch = {
 const templeGrand= {
     id: 'Temple Grandin: How the Girl Who Loved Cows Embraced Autism and Changed the World by Sy Montgomery',
     name: 'Temple Grandin: How the Girl Who Loved Cows Embraced Autism and Changed the World',
-    image: 'assets/templeGran.png',
+    image: '../assets/templeGran.png',
     description: 'When Temple Grandin was born, her parents knew she was different. Years later she was diagnosed with autism. Temple’s doctor recommended institutionalizing her, but her mother believed in her. Temple went to school instead. Today, Dr. Temple Grandin, a scientist and professor of animal science at Colorado State University, is an autism advocate and her world-changing career revolutionized the livestock industry. This compelling biography and Temples personal photos take us inside her extraordinary mind and open the door to a broader understanding of autism.',
     category: 'Biographical-Farming',
     price: 16.95
@@ -33,15 +33,15 @@ const templeGrand= {
 const theStand = {
     id: 'The Stand by Stephen King',
     name: 'The Stand',
-    image: 'assets/theStand.png',
+    image: '../assets/theStand.png',
     description: 'A monumentally devastating plague leaves only a few survivors who, in a desert world, experience dreams of good and evil in confrontation and, through their choices, move toward an actual confrontation',
     category: 'Horror',
     price: 18.00
 };
-const cantHurtMe  = {
+const cantHurtMe = {
     id: 'Cant Hurt Me: Master Your Mind and Defy the Odds by David Goggins',
     name: 'Cant Hurt Me: Master Your Mind and Defy the Odds',
-    image: 'assets/cantHurtMe.png',
+    image: '../assets/cantHurtMe.png',
     description: 'In Cant Hurt Me, he shares his astonishing life story and reveals that most of us tap into only 40% of our capabilities. Goggins calls this The 40% Rule, and his story illuminates a path that anyone can follow to push past pain, demolish fear, and reach their full potential.',
     category: 'self-help',
     price: 13.98
