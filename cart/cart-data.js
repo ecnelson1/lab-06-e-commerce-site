9607106c0caff7fd7325dd07dbe7ee58871f6087
Eion Nelson <ecnelson1@live.com>
2021-01-20 15:03:51 -0800

    added cart data

---
+++ cart/cart-data.js
@@ -0,0 +1,13 @@
+const cart = [{
+    id: 'The Midnight Library by Matt Haig',
+    quantity:1
+},
+{
+    id: 'Notorious RBG by Irin Carmon and Shana Knizhnik',
+    quantity:3
+},
+{
+    id: 'Bucklands Complete Book of Witchcraft by Raymond Buckland',
+    quantity:7
+}];
+export default cart;
